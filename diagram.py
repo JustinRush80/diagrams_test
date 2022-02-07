c5fce54915d5fd65cf375e15ab8c3089474e6907
JustinRush80 <69156844+JustinRush80@users.noreply.github.com>
2022-02-07 08:49:08 -0500

    update

--- diagram.py
+++ diagram.py
@@ -1,10 +1,40 @@
-from diagrams import Diagram
-from diagrams.aws.compute import Lambda
+from diagrams import Diagram,Cluster
 from diagrams.aws.database import RDS
 from diagrams.programming.language import Python
 from diagrams.onprem.compute import Server
+from diagrams.generic.storage import Storage
+from diagrams.onprem.ci import GitlabCI
+from diagrams.onprem.analytics import Tableau
+from diagrams.onprem.client import Users
 
 
-with Diagram("pic/ICPS ETL", show=False):
-    Server("ICPS") >> Server("ODS") >> Python() >> RDS("ods_dev")
+
+
+with Diagram(name="ICPS Scenario Evaluation", filename= "pic/icps_etl", show=True):
+    graph_attrs = {
+        "shape": "box",
+        "style": "dashed",
+        "labeljust": "l",
+        "pencolor": "#AEB6BE",
+        "fontname": "Times-Roman",
+        "fontsize": "14",
+    }
+    
+ 
+    with Cluster("Check and transfer new files every minute",graph_attr=graph_attrs):
+        with Cluster("Direct access only for planners") as c:
+            icps =  Storage("ICPS")
+            Users("Planners") >> icps
+        server = Server("sxnda82724")
+        icps >> server
+        
+        
+        with Cluster("new files triggers script", graph_attr=graph_attrs):
+            script = Python()
+            server >> script
+                
+
+    server >> RDS("Back up Files")
+    script >> Tableau()
+    
 
